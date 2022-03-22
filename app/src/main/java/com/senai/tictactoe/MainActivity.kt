5d491c72b39b6fc110939578f11d384a751ee620
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2022-03-22 16:56:05 -0300

    atividade dificuldade e escolha de token adicionadas

--- app/src/main/java/com/senai/tictactoe/MainActivity.kt
+++ app/src/main/java/com/senai/tictactoe/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.senai.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pvp = findViewById<Button>(R.id.pvpButton)
+
+        pvp.setOnClickListener{
+            val intent = Intent(this, TokenChooseActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
